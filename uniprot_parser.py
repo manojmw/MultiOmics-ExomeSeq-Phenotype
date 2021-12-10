93a58fa3195ceb2ffff83f6e96faaa357c7cf45d
Manoj M Wagle <74168582+manojmw@users.noreply.github.com>
2021-12-10 16:47:01 +0100

    Update uniprot_parser.py

--- uniprot_parser.py
+++ uniprot_parser.py
@@ -3,9 +3,10 @@
 import re
 import csv
 
-###Creating output file and opening it for writing ----- ****To be completed later**** --------
+###Creating output file and opening it for reading ----- ****To be completed later**** --------
 outputFile1 = open("uniprot_results_main.tsv", "w")
 outputFile2 = open("uniprot_results_secondary.tsv", "w")
+
 try:
     with open("uniprot_results.tsv", 'w', newline = '') as tsv_out:
         csv_writer = csv.writer(tsv_out, delimiter = '\t')
@@ -13,68 +14,89 @@ try:
         csv_writer.writerow(header)
 
     ###Opening the file in the read mode
-    uniprotfile = open('/path-to-the-file/', 'r')
+    uniprotfile = open('/Users/macbookpro/Desktop/MBHE/Internship/Data/uniprot_50000.dat', 'r')
 
     ###Initializing variables
     ACs = ''
-    Organism = []
+    OS = ''
     ENSTs = []
     ENSGs = []
     GeneIDs = []
 
+    ###Compiling all the regular expressions###
+
+    ###Compiling Regex for the Accession Number (AC) from the AC line
+    re_AC = re.compile('^AC\s+(\S.*)')
+    ###Compliling Regex for the Organism from the OS line
+    re_OS = re.compile('^OS\s+(.*)')
+    ###Compliling Regex for the the Ensemble transcripts and Genes from the DR line
+    re_ENS1 = re.compile('^DR\s+Ensembl;\s+(ENST\d+);\sENSP\d+;\s+(ENSG\d+)\.')
+    ###Some records contain only ENSGs with the below regex pattern
+    ####We will Compile Regex for the lines that contain only Ensembl Genes from the DR line
+    re_ENS2 = re.compile('^DR\s+Ensembl;\s+(ENSG.*)\.')
+    ###Compliling Regex for the the Gene IDs from the DR line
+    re_GID = re.compile('^DR\s+GeneID;\s+(\d+);')
+
     for line in uniprotfile:
-        line = line.rstrip() ##removing any trailing spaces
+        line = line.rstrip('\r\n') ##removing new lines and carriage returns
 
-        ###Matching and retrieving the Accession Number (AC)
-        AC_match = re.search(r'^AC\s+(\S.*)', line)
-        AC_exists = re.search(r'^AC\s+', line) ##If any AC line is missed, we will use this to break the loop
-
-        ###Matching and retrieving Organism from the OS line
-        Organism_match = re.search(r'^OS\s+(.*)\.', line)
-
-        ###Matching and retrieving the Ensemble transcript and Genes
-        ENS_match1 = re.search(r'^DR\s+Ensembl;\s+(ENST\d+);\sENSP\d+;\s+(ENSG\d+)\.', line)
-        ENS_exists1 = re.search(r'^DR\s+Ensembl;\s+ENST\d+', line) ##If any DR line with ENSTs/ENSGs of the above pattern is missed, we will use this to break the loop
-
-        ###Some records contain only ENSGs with the below regex pattern
-        ####We will Match and retrieve lines that contain only Ensembl Genes
-        ENS_match2 = re.search(r'^DR\s+Ensembl;\s+(ENSG.*)\.', line)
-        ENS_exists2 = re.search(r'^DR\s+Ensembl;\sENSG.*', line) ##If any DR line with ENSGs of the above pattern is missed, we will use this to break the loop
-
-        ###Matching and retrieving the Gene IDs
-        GeneID_match = re.search(r'^DR\s+GeneID;\s+(\d+);', line)
-        GeneID_exists = re.search(r'^DR\s+GeneID.*', line) ##If any DR line with GeneIDs of the above pattern is missed, we will use this to break the loop
-
-        if AC_match:
-            ACs += AC_match.group(1) + ' '
-        elif AC_exists:
-            print("###Oops....Missed the AC line", AC_exists)
+        ###Matching and retrieving the records
+        if (re_AC.match(line)):
+            ACs += re_AC.match(line).group(1) + ' '
+        elif (re.match(r'^AC\s+', line)): ##If any AC line is missed, we will use this to break the loop
+            print("###Oops....Missed the AC line %s\n", line)
             break
-        elif Organism_match:
-            Organism.append(Organism_match.group(1))
-        elif ENS_match1:
-            ENSTs.append(ENS_match1.group(1))
-            ENSGs.append(ENS_match1.group(2))
-        elif ENS_match2:
-            ENSG2 = ENS_match2.group(1)
+        elif (re_OS.match(line)):
+            OS += re_OS.match(line).group(1) + '; '
+        elif (re.match(r'^OS\s+', line)):
+            print("###Oops....Missed the OS line %s\n", line)
+            break
+        elif (re_ENS1.match(line)):
+            ENSTs.append(re_ENS1.match(line).group(1))
+            ENSGs.append(re_ENS1.match(line).group(2))
+        elif (re_ENS2.match(line)):
+            ENSG2 = re_ENS2.match(line).group(1)
             if ENSG2 not in ENSGs:
                 ENSGs.append(ENSG2)
-        elif ENS_exists1 or ENS_exists2:
-            print("###Oops....Failed to get all the Ensembl Identifiers", ENS_exists1, ENS_exists2)
+        elif (re.match(r'^DR\s+Ensembl;\s+(ENSG.*)\.', line) or re.match(r'^DR\s+Ensembl;\sENSG.*', line)): ##If any DR line with ENSTs/ENSGs is missed, we will use this to break the loop
+            print("###Oops....Failed to get all the Ensembl Identifiers %s\n", ENS_exists1, ENS_exists2)
             break
-        elif GeneID_match:
-            GeneIDs.append(GeneID_match.group(1))
-        elif GeneID_exists:
-            print("###Oops....Missed the GeneIDs", GeneID_exists)
+        elif (re_GID.match(line)):
+            GeneIDs.append(re_GID.match(line).group(1))
+        elif (re.match(r'^DR\s+GeneID.*', line)): ##If any DR line with GeneIDs is missed, we will use this to break the loop
+            print("###Oops....Missed the GeneIDs %s\n", GeneID_exists)
             break
-
         ###Processing the matched records of the protein
         elif (line == '//'):
             try:
-                primary_AC = re.search(r'^(^;\s)+').group(1)
+                AC_split = ACs.split('; ')
+                primary_AC = str(AC_split[:1])
+                secondary_AC = str(AC_split[1:-1])
+                ACs = ''
             except:
-                print('###Oops...Failed to get Primary Accession')
+                print('###Oops...Failed to get Accession IDs')
                 break
+            try:
+                OS_split = OS.split(';')
+                Organism = str(OS_split[:1])
+                OS = ''
+            except:
+                print('###Oops...Failed to get the Organism name')
+                break
+            try:
+                ENSTs_split = ENSTs.split(',')
+                primary_ENST = str(ENSTs_split[:1])
+                alternate_ENST = str(ENSTs_split[1:-1])
+                ENSGs_split = ENSGs.split(',')
+                primary_ENSG = str(ENSGs_split[:1])
+                alternate_ENST = str(ENSGs_split[1:-1])
+                ENSTs = []
+                ENSGs = []
+            except:
+                print('###Oops...Failed to get Ensembl Identifiers')
+                break
+
+
 
 
 except IOError as e:
