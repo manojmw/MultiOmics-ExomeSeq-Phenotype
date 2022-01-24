f40d8e7bf18ba70cb89cd1e468059cb136d8eecf
manojmw <manojmonmax@gmail.com>
2022-01-24 17:50:53 +0100

    Code improvements

--- interaction_parser.py
+++ interaction_parser.py
@@ -3,12 +3,13 @@
 import re
 import csv
 import argparse
+import sys
 
 def interaction_parser(args):
     try:
-        with open("biogrid.tsv", 'w', newline = '') as tsv_out:
+        with open(args.output, 'w', newline = '') as tsv_out:
             csv_writer = csv.writer(tsv_out, delimiter = '\t')
-            header = ['Protein_A', 'Protein_B', 'Method', 'PMID', 'TaxID_A', 'TaxID_B', 'Interaction_type']
+            header = ['Protein_A', 'Protein_B', 'Method', 'PMID', 'Interaction_type']
             csv_writer.writerow(header)
 
             ###Creating dictionaries from the uniprot output files###
@@ -21,7 +22,7 @@ def interaction_parser(args):
 
             for line in Primary_AC_file:
                 Primary_AC_fields = line.split('\t')
-                (key,value) = (Primary_AC_fields[0], 1) ##Key -> primary accession
+                (key,value) = (Primary_AC_fields[0], Primary_AC_fields[1]) ##Key -> primary accession
                 Primary_AC_dict[key] = value
 
             ###UniProt Secondary Accession dictionary
@@ -34,9 +35,14 @@ def interaction_parser(args):
                 line = line.rstrip("\n") ##removing carriage returns
                 Secondary_AC_fields = line.split('\t')
                 (key,value) = (Secondary_AC_fields[1], Secondary_AC_fields[0]) ##Key -> secondary accession
-                Secondary_AC_dict[key] = value
+                if Secondary_AC_dict.get(key, False):
+                    if Secondary_AC_dict[key] != "-1":
+                        Secondary_AC_dict[key]  = "-1"
+                    #else: Secondary_AC is already bad => NOOP
+                else:
+                    Secondary_AC_dict[key] = value
 
-            ###UniProt GeneID dictionary
+            ###GeneID dictionary
             GeneID_dict = {} ##Initializing an empty dictionary
             GeneID_file = open(args.input4)
 
@@ -54,14 +60,6 @@ def interaction_parser(args):
             ###Skip header
             interaction_file.readline()
 
-            ###Initializing variables/accumulators
-            Prots = ['','']
-            Method = ''
-            PMID = 0
-            TaxID_A = 0
-            TaxID_B = 0
-            Interaction_type = ''
-
             ###Compiling all the regular expressions###
 
             ###uniprot ids for protein
@@ -74,85 +72,62 @@ def interaction_parser(args):
                 line = line.rstrip('\n')
                 line_fields = line.split('\t')
 
+                ###Initializing variables/accumulators
+                Prots = ['','']
+                Method = ''
+                PMID = 0
+                Interaction_type = ''
+
                 for protindex in [0,1]:
                     if (re_uniprot.match(line_fields[protindex])):
                         ID = re_uniprot.match(line_fields[protindex]).group(2)
                         ##Check if it exists in the dictionary
-                        if ID in Primary_AC_dict:
+                        if Primary_AC_dict.get(ID, False):
                             Prots[protindex] = ID
+                            continue
                     elif (re_uniprot_missed.match(line_fields[protindex])):
-                        print("ID is a uniprot Accession but failed to grab it", line)
-                        break
-                if (Prots[0] == ''): ###If the Protein_A Accession ID is not found, search in the 3rd column
-                    line_fields_split = line_fields[2].split('|')
-                    if (re_uniprot.match(line_fields_split)):
-                        ID = re_uniprot.match(line_fields_split).group(2)
+                        sys.exit("ID is a uniprot Accession but failed to grab it\n" + line)
+                    elif (re_GeneID.match(line_fields[protindex])):
+                        ID = re_GeneID.match(line_fields[protindex]).group(1)
                         ##Check if it exists in the dictionary
-                        if ID in Primary_AC_dict:
-                            Prots[0] = ID
-                        ###ElseIf the accession is found in the Secondary_AC_dict
-                        elif ID in Secondary_AC_dict:
-                            Prots[0] = Secondary_AC_dict.get(ID) ###Get the corresponding Primary Uniprot Accession ID
-                    elif (re_uniprot_missed.match(line_fields_split)):
-                        print("ID is a uniprot Accession but failed to grab it", line)
-                        break
-                elif (Prots[1] == ''): ###If the Protein_B Accession ID is not found, search in the 4th column
-                    line_fields_split = line_fields[3].split('|')
-                    if (re_uniprot.match(line_fields_split)):
-                        ID = re_uniprot.match(line_fields_split).group(2)
-                        ##Check if it exists in the dictionary
-                        if ID in Primary_AC_dict:
-                            Prots[1] = ID
-                        ###ElseIf the accession is found in the Secondary_AC_dict
-                        elif ID in Secondary_AC_dict:
-                            Prots[1] = Secondary_AC_dict.get(ID) ###Get the corresponding Primary Accession
-                    elif (re_uniprot_missed.match(line_fields_split)):
-                        print("ID is a uniprot Accession but failed to grab it", line)
-                        break
-                elif (Prots[0] == ''): ###If the Protein_A Accession ID is not found, search in the 5th column
-                    line_fields_split = line_fields[4].split('|')
-                    if (re_uniprot.match(line_fields_split)):
-                        ID = re_uniprot.match(line_fields_split).group(2)
-                        ##Check if it exists in the dictionary
-                        if ID in Primary_AC_dict:
-                            Prots[0] = ID
-                        ###ElseIf the accession is found in the Secondary_AC_dict
-                        elif ID in Secondary_AC_dict:
-                            Prots[0] = Secondary_AC_dict.get(ID) ###Get the corresponding Primary Uniprot Accession ID
-                    elif (re_uniprot_missed.match(line_fields_split)):
-                        print("ID is a uniprot Accession but failed to grab it", line)
-                        break
-                elif (Prots[1] == ''): ###If the Protein_B Accession ID is not found, search in the 5th column
-                    line_fields_split = line_fields[5].split('|')
-                    if (re_uniprot.match(line_fields_split)):
-                        ID = re_uniprot.match(line_fields_split).group(2)
-                        ##Check if it exists in the dictionary
-                        if ID in Primary_AC_dict:
-                            Prots[1] = ID
-                        ###ElseIf the accession is found in the Secondary_AC_dict
-                        elif ID in Secondary_AC_dict:
-                            Prots[1] = Secondary_AC_dict.get(ID) ###Get the corresponding Primary Accession
-                    elif (re_uniprot_missed.match(line_fields_split)):
-                        print("ID is a uniprot Accession but failed to grab it", line)
-                        break
-                elif (Prots[0] == ''): ###If the Protein_A Uniprot Accession ID is not found, search using GeneID
-                    if (re_GeneID.match(line_fields[0])):
-                        ID = re_GeneID.match(line_fields[0]).group(1)
-                        ##Check if it exists in the GeneID_dict
-                        if ID in GeneID_dict:
-                            Prots[0] = GeneID_dict.get(ID) ###Get the corresponding Primary Uniprot Accession ID
-                    elif (re_GeneID_missed.match(line_fields[0])):
-                        print("Failed to grab GeneID for the line: ", line)
-                        break
-                elif (Prots[1] == ''): ###If the Protein_A Uniprot Accession ID is not found, search using GeneID
-                    if (re_GeneID.match(line_fields[1])):
-                        ID = re_GeneID.match(line_fields[1]).group(1)
-                        ##Check if it exists in the GeneID_dict
-                        if ID in GeneID_dict:
-                            Prots[1] = GeneID_dict.get(ID) ###Get the corresponding Primary Uniprot Accession ID
-                    elif (re_GeneID_missed.match(line_fields[1])):
-                        print("Failed to grab GeneID for the line: ", line)
-                        break
+                        if GeneID_dict.get(ID, False):
+                            Prots[protindex] = GeneID_dict[ID]
+                            continue
+                    elif (re_GeneID_missed.match(line_fields[protindex])):
+                        sys.exit("ID is a GeneID but failed to grab it\n" + line)
+
+                    ###Uniprot AC not found/not primary_AC and GeneID not found, look in Alternate ID columns
+                    altIDs = line_fields[2+protindex].split('|')
+                    for altID in altIDs:
+                        if (re_uniprot.match(altID)):
+                            ID = re_uniprot.match(altID).group(2)
+                            ##Check if it exists in the dictionary
+                            if Primary_AC_dict.get(ID, False):
+                                if Prots[protindex] == '':
+                                    Prots[protindex] = ID
+                                    continue
+                                else:
+                                    continue next line
+                            ###ElseIf the accession is found in the Secondary_AC_dict
+                            elif Secondary_AC_dict.get(ID, "-1") != "-1":
+                                if Prots[protindex] == '':
+                                    Prots[protindex] = Secondary_AC_dict[ID] ###Get the corresponding Primary Uniprot Accession ID
+                                    continue
+                                else:
+                                    continue next line
+                        elif (re_uniprot_missed.match(altID)):
+                            sys.exit("ID is a uniprot Accession but failed to grab it\n" + line)
+                        elif (re_GeneID.match(altID)):
+                            ID = re_GeneID.match(altID).group(1)
+                            ##Check if it exists in the dictionary
+                            if GeneID_dict.get(ID, False):
+                                if Prots[protindex] == '':
+                                    Prots[protindex] = GeneID_dict[ID]
+                                    continue
+                                else:
+                                    continue next line
+                        elif (re_GeneID_missed.match(altID)):
+                            sys.exit("ID is a GeneID but failed to grab it\n" + line)
 
 
 
@@ -164,17 +139,17 @@ def interaction_parser(args):
 
 
 
-        ###Closing the written files
-        tsv_out.close()
+
+            ###Closing the written files
+            tsv_out.close()
 
     except IOError as e:
-        print("Error: Unable to open the file for writing")
+            print("Error: Unable to open the file for writing")
 
 ####Taking and handling command-line arguments
 def main():
-    file_parser = argparse.ArgumentParser(description = "Program: Parses a Protein-protein Interaction (PPI) file, maps to the uniprot file and produces the necessary output files")
     formatter = lambda prog: argparse.HelpFormatter(prog, max_help_position=70)
-    file_parser = argparse.ArgumentParser(formatter_class=formatter)
+    file_parser = argparse.ArgumentParser(formatter_class=formatter, description = "Program: Parses a Protein-protein Interaction (PPI) file, maps to the uniprot file and produces the necessary output files")
 
     required = file_parser.add_argument_group('Required arguments')
     optional = file_parser.add_argument_group('Optional arguments')
