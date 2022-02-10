d3835955c994589b32175d9823de11e8ed3ef477
manojmw <manojmonmax@gmail.com>
2022-02-10 20:48:50 +0100

    Modifed Final QC, same key for iterating 2 dict, remove zip

--- 4_Interactome.py
+++ 4_Interactome.py
@@ -37,44 +37,45 @@ def IntPMID(curatedIntfile):
 
             ##Key -> UniProt PrimAC of Protein A & B joined together by an '_'
             ##Value -> Pubmed Identifier (PMID) - curatedPPI_fields[3]
-            (Int_key1, PMID) = (Interactors, curatedPPI_fields[3])
+            (Int_key, PMIDs) = (Interactors, curatedPPI_fields[3])
 
             ##Check if the Key exists in PPI_PMID_dict
             ##If yes, then store the values (PMIDs) as a list
-            if PPI_PMID_dict.get(Int_key1, False):
-                if PMID not in PPI_PMID_dict[Int_key1]: ###Avoiding duplicate PMIDs
-                    PPI_PMID_dict[Int_key1].append(PMID)
+            if PPI_PMID_dict.get(Int_key, False):
+                if PMIDs not in PPI_PMID_dict[Int_key]: ###Avoiding duplicate PMIDs
+                    PPI_PMID_dict[Int_key].append(PMIDs)
             else:
-                PPI_PMID_dict[Int_key1] = [PMID]
+                PPI_PMID_dict[Int_key] = [PMIDs]
 
             ##Key -> UniProt PrimAC of Protein A & B joined together by an '_'
             ##Value -> Interaction Detection Method - curatedPPI_fields[2]
-            (Int_key2, IntDetMeth) = (Interactors, curatedPPI_fields[2])
+            (Int_key, IntDetMeth) = (Interactors, curatedPPI_fields[2])
 
-            if PPI_IntDetMethod_dict.get(Int_key2, False):
-                PPI_IntDetMethod_dict[Int_key2].append(IntDetMeth)
+            if PPI_IntDetMethod_dict.get(Int_key, False):
+                PPI_IntDetMethod_dict[Int_key].append(IntDetMeth)
             else:
-                PPI_IntDetMethod_dict[Int_key2] = [IntDetMeth]
+                PPI_IntDetMethod_dict[Int_key] = [IntDetMeth]
 
     ###Processing the dictionaries and printing to STDOUT
-    for (Int_key1,PMID), (Int_key2,IntDetMeth) in zip(PPI_PMID_dict.items(), PPI_IntDetMethod_dict.items()):
+    ##Since both the dictionaries have the same keys,
+    ###We can use the keys from PPI_PMID_dict to iterate through PPI_IntDetMethod_dict
+    for Int_key in PPI_PMID_dict:
 
         ###Checking if at least one of the experiments for a given interaction has been proved by any binary interaction method
         ###i.e. Other than Affintity chromatography technology (ACT) - 'MI:0004'
         ###Used to eliminate PPIs that have been proved ONLY by using ACT
-        if (any(value != 'MI:0004' for value in PPI_IntDetMethod_dict[Int_key2])):
-            Proteins = Int_key1.split('_')
+        if (any(value != 'MI:0004' for value in PPI_IntDetMethod_dict[Int_key])):
+            Proteins = Int_key.split('_')
             Protein_A = Proteins[0]
             Protein_B = Proteins[1]
 
-            Pubmed_Identifier = ', '.join(PPI_PMID_dict[Int_key1])
-            PMID_count = len(PPI_PMID_dict[Int_key1])
-            Exp_count = len(PPI_IntDetMethod_dict[Int_key2])
+            Pubmed_Identifier = ', '.join(PPI_PMID_dict[Int_key])
+            PMID_count = len(PPI_PMID_dict[Int_key])
+            Exp_count = len(PPI_IntDetMethod_dict[Int_key])
 
             ###Final Quality Control
-            ##At least 2 publications for a given PPI
-            ##OR if it is one publication, then at least 3 experiments
-            if ((PMID_count >= 2) or (PMID_count == 1 and Exp_count >= 3)):
+            ##Each interaction has at least 2 experiments
+            if Exp_count >= 2:
                 interaction_out_line = (Protein_A, Protein_B, str(PMID_count), Pubmed_Identifier, str(Exp_count))
                 print('\t'.join(interaction_out_line))
 
