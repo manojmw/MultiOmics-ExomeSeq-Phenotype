beb78cee8d0267d2d9e4a16caf26c0689c6dbd6f
manojmw <manojmonmax@gmail.com>
2022-02-15 19:42:53 +0100

    Comment fix

--- 3_check_HumanPPIExp.py
+++ 3_check_HumanPPIExp.py
@@ -3,30 +3,32 @@
 import re
 import argparse, sys
 
-###Program to check the number of Human-Human Protein Interaction experiments
-
+###Function to check the number of Human-Human Protein Interaction experiments###
+# Takes miTAB 2.5 or 2.7 file as input
+# Keeps the count of experiments where both proteins have TaxID = 9606 i.e Human
+# Prints the total no. of Human-Human Protein Interaction experiments to STDOUT
 def checkHumanPPI(interaction_file):
 
-    ##User input -> Protein-protein Interaction file
+    # User input -> Protein-protein Interaction file
     interaction_file = sys.stdin
 
-    ###Skip header
+    # Skip header
     interaction_file.readline()
 
-    ###Keeping the count of Human-Human Interaction experiments
+    # Keeping the count of Human-Human Interaction experiments
     HumanInt_Count = 0
 
-    ###Compiling regular expression###
+    # Compiling regular expression
 
-    ###TaxID id for protein
+    # TaxID id for protein
     re_taxID = re.compile('^taxid:(9606)')
 
-    ###Parsing the interaction file
+    # Parsing the interaction file
     for line in interaction_file:
         line = line.rstrip('\n')
         line_fields = line.split('\t')
 
-        ###Initializing accumulators
+        # Initializing accumulators
         TaxIDs = ['','']
 
         for taxindex in [0,1]:
@@ -39,15 +41,15 @@ def checkHumanPPI(interaction_file):
             continue
     print("\nTotal Number of Human-Human Protein Interaction experiments: ", HumanInt_Count, "\n")
 
-    ###Closing the file
+    # Closing the file
     interaction_file.close()
 
-####Taking and handling command-line arguments
+# Taking and handling command-line arguments
 def main():
     file_parser = argparse.ArgumentParser(description =
     """
 ----------------------------------------------------------------------------------------------------------------------
-Program: Parses a MITAB 2.5 or 2.7 file and prints the number of Human-Human Protein Interaction experiments to STDOUT
+Program: Parses a miTAB 2.5 or 2.7 file and prints the number of Human-Human Protein Interaction experiments to STDOUT
 ----------------------------------------------------------------------------------------------------------------------
 Usage:
 
