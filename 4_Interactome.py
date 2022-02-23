d08c5f2afa91df55f5c6f91b71392be7020acd5f
manojmw <manojmonmax@gmail.com>
2022-02-23 13:45:59 +0100

    IntPMID - header removed, returns a list and add new function for parsing ENSG & Gene

--- 4_Interactome.py
+++ 4_Interactome.py
@@ -2,21 +2,23 @@
 
 import sys, argparse
 
-###Function for building the high-quality INTERACTOME###
-# Takes the output files produced by the interaction_parser.py as INPUT
-# This file consists of five columns i.e.
-# Protein A UniProt PrimAC, Protein B UniProt PrimAC, Interaction Detection Method, Pubmed Identifier and Interaction type
+###########################################################
+
+# Parses output files produced by the interaction_parser.py
+# Required columns are: Protein A UniProt PrimAC, Protein B UniProt PrimAC,
+# Interaction Detection Method, Pubmed Identifier and Interaction type
+#
 # Processes it by filtering based on Interaction Detection Method and Interaction type
 # Final filtering - Each interaction has at least 2 experiments
-# Prints to STDOUT
-# OUTPUT is a High-quality Human Interactome that consists of five columns in .tsv format
-# i.e., UniProt PrimAC, Protein B UniProt PrimAC, Publication_Count, PMID(s) and Experiment_count
+# Returns a list with 5 items (in each sublist):
+# - Protein A UniProt PrimAC,
+# - Protein B UniProt PrimAC,
+# - Publication_Count,
+# - PMID(s) and
+# - Experiment_count
 def IntPMID(curatedIntfile):
 
-    header = ('Protein_A_UniprotPrimAC', 'Protein_B_UniprotPrimAC', 'Publication_Count', 'Publication_Identifier(s)', 'Experiment_count')
-    print('\t'.join(header))
-
-    PPI_PMID_dict = {} # Dictionary for interactions
+    PPI_PMID_dict = {} # Dictionary for PPIs
     PPI_IntDetMethod_dict = {} # Dictionary for experiments and filtering based on IntDetMethod
 
     curatedIntfile = sys.stdin
@@ -63,7 +65,10 @@ def IntPMID(curatedIntfile):
             else:
                 PPI_IntDetMethod_dict[Int_key] = [IntDetMeth]
 
-    # Processing the dictionaries and printing to STDOUT
+    # Initializing output list
+    Interactome_list = []
+
+    # Processing the dictionaries and returning a list
     # Since both the dictionaries have the same keys,
     # We can use the keys from PPI_PMID_dict to iterate through PPI_IntDetMethod_dict
     for Int_key in PPI_PMID_dict:
@@ -83,11 +88,58 @@ def IntPMID(curatedIntfile):
             # Final Quality Control
             # Each interaction has at least 2 experiments
             if Exp_count >= 2:
-                interaction_out_line = (Protein_A, Protein_B, str(PMID_count), Pubmed_Identifier, str(Exp_count))
-                print('\t'.join(interaction_out_line))
+                interaction_out_line = [Protein_A, Protein_B, str(PMID_count), Pubmed_Identifier, str(Exp_count)]
+                Interactome_list.append(interaction_out_line)
 
-    return
+    return Interactome_list
 
+###########################################################
+
+# Parses tab-seperated canonical transcripts file
+# Required columns are: 'ENSG' and 'GENE' (can be in any order,
+# but they MUST exist)
+# Returns a dictionary:
+# Key -> ENSG; Value -> Gene
+def ENSG_Gene(inCanonicalFile):
+
+    ENSG_Gene_dict = {} # Initializing an empty dictionary
+
+    Canonical_File = open(inCanonicalFile)
+
+    Canonical_header_line = Canonical_File.readline() # Grabbing the header line
+
+    Canonical_header_fields = Canonical_header_line.split('\t')
+
+    # Check the column headers and grab indexes of our columns of interest
+    (ENSG_col, Gene_col) = (-1,-1)
+
+    for header in Canonical_header_fields:
+        if header == 'ENSG':
+            ENSG_col = Canonical_header_fields.index(header)
+        elif header == 'GENE':
+            Gene_col = Canonical_header_fields.index(header)
+
+    if not ENSG_col >= 0:
+        sys.exit("Missing required column title 'ENSG' in the file: %s \n" % inCanonicalFile)
+    elif not Gene_col >= 0:
+        sys.exit("Missing required column title 'GENE' in the file: %s \n" % inCanonicalFile)
+    # else grabbed the required column indexes -> PROCEED
+
+    # Parsing the Uniprot Primary Accession file
+    for line in Canonical_File:
+        line = line.rstrip('\n')
+        CanonicalTranscripts_fields = line.split('\t')
+
+        # Key -> ENSG
+        # Value -> Gene
+        (ENSG_key, Gene) = (CanonicalTranscripts_fields[ENSG_col], CanonicalTranscripts_fields[Gene_col])
+        ENSG_Gene_dict[ENSG_key] = Gene
+
+    return ENSG_Gene_dict
+
+
+
+###########################################################
 
 # Taking and handling command-line arguments
 def main():
@@ -103,8 +155,8 @@ Usage:
 -> curatedPPI_files: output files produced by the interaction_parser.py
 
 The output (High-quality Human Interactome) consists of five columns in .tsv format:
-  -> UniProt Primary Accession of Protein A
-  -> UniProt Primary Accession of Protein B
+  -> ENSG of Protein A
+  -> ENSG of Protein B
   -> Number of Publications associated with the interaction of the above 2 proteins
   -> PMID (or a comma seperated list of PMIDs)
   -> Count of Experiments for each interaction
