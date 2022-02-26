970860daedba5702b65326151a3f9a8fe1d76ba0
manojmw <manojmonmax@gmail.com>
2022-02-26 14:46:18 +0100

    Closing the file, comment fix for CandidateGene2ENSG function and description fix in --help

--- 7_Lead-1_candidateGenes.py
+++ 7_Lead-1_candidateGenes.py
@@ -10,8 +10,10 @@ import time
 # Parses tab-seperated canonical transcripts file
 # Required columns are: 'ENSG' and 'GENE' (can be in any order,
 # but they MUST exist)
+#
 # Returns a dictionary:
-# Key -> Gene; Value -> ENSG
+# Key -> Gene
+# Value -> ENSG
 def ENSG_Gene(inCanonicalFile):
 
     ENSG_Gene_dict = {} # Initializing an empty dictionary
@@ -46,14 +48,22 @@ def ENSG_Gene(inCanonicalFile):
         # Value -> ENSG
         ENSG_Gene_dict[CanonicalTranscripts_fields[Gene_col]] = CanonicalTranscripts_fields[ENSG_col]
 
+    # Closing the file
+    Canonical_File.close()
+
     return ENSG_Gene_dict
 
 ###########################################################
 
-###Function for extracting data from candidate gene file###
-# Takes candidate gene file in .xlsx format as INPUT
-# Extracts data from 3 columns - Gene, pathologyID and Confidence score
-# Returns a DataFrame with the data from above 3 columns - Candidate_data
+# Parses the candidateGenes file in .xlsx format
+# Required columns are: 'Gene', 'pathologyID' and
+# 'Confidence score' (can be in any order,
+# but they MUST exist)
+#
+# Returns a list with 3 items (in each sublist:
+# - Gene
+# - pathologyID
+# - Confidence score)
 def CandidateGeneParser(inCandidateFile):
 
     # Input - List of candidate gene file(s)
@@ -74,12 +84,16 @@ def CandidateGeneParser(inCandidateFile):
 
 ###########################################################
 
-###Function for mapping Candidate Genes to canonical ENSGs###
+# Parses the dictionary (ENSG_Gene_dict) returned
+# by the function: ENSG_Gene and the list (CandidateGene_data)
+# returned by the function: CandidateGeneParser
 # Calls the functions ENSG_Gene and CandidateGeneParser
-# Parses the input files as described in the respective functions
-# Maps the Genes to ENSGs
-# Prints to STDOUT in .tsv format
-# Output consists of 3 columns: ENSG, PathologyID and Confidence Score
+#
+# Maps Gene name to ENSG
+# Returns a list with 3 items (in each sublist:
+# - ENSG
+# - pathologyID
+# - Confidence score)
 def CandidateGene2ENSG(inCanonicalFile, inCandidateFile):
 
     # Calling the function ENSG_Gene
@@ -131,6 +145,9 @@ def Interacting_Proteins(inInteractome):
         Interacting_Proteins = [Interactome_fields[0], Interactome_fields[1]]
 
         Interactome_list.append(Interacting_Proteins)
+
+    # Closing the file
+    Interactome_File.close()
 
     return Interactome_list
 
@@ -191,8 +208,10 @@ def Lead1_CandidateENSG(inCanonicalFile, inCandidateFile, inInteractome):
 
 ###########################################################
 
-# Parses the candGene_Interactors_list returned by the function: Lead1_CandidateENSG
+# Parses the list (candGene_Interactors_list)
+# returned by the function: Lead1_CandidateENSG
 # Maps the ENSGs in the list to Gene names using the dictionary: ENSG_Gene_dict
+#
 # Prints to STDOUT in .tsv format
 # Output consists of 6 columns:
 # - Candidate Gene
@@ -234,21 +253,26 @@ def Lead1_CandidateGene(args):
 def main():
     file_parser = argparse.ArgumentParser(description =
     """
---------------------------------------------------------------------------------------------------------------------
-Program: Parses the patient Candidate Gene file(s) and Canonical Transcripts file, processes it and prints to STDOUT
---------------------------------------------------------------------------------------------------------------------
-The output consists of 3 columns in .tsv format:
- -> Candidate ENSG
+-----------------------------------------------------------------------------------------------------------------------
+Program: Parses the patient Candidate Gene file(s) and Canonical Transcripts file. Using the Interactome file generated
+         by Interactome.py, checks the number of interactors for each candidate gene. Next, checks the number of
+         interactors that are known candidate genes and prints to STDOUT in .tsv format
+-----------------------------------------------------------------------------------------------------------------------
+The output consists of 6 columns in .tsv format:
+ -> Candidate Gene
  -> Pathology Identifier
  -> Confidence score
---------------------------------------------------------------------------------------------------------------------
+ -> Number of Interactors with the candidate gene
+ -> Number of Interactors that are known candidate genes
+ -> A comma-seperated list of these known Interactors
+-----------------------------------------------------------------------------------------------------------------------
     """,
     formatter_class = argparse.RawDescriptionHelpFormatter)
 
     required = file_parser.add_argument_group('Required arguments')
     optional = file_parser.add_argument_group('Optional arguments')
 
-    required.add_argument('--inCandidateFile', metavar = "Input File", dest = "inCandidateFile", nargs = '+', help = 'Input File Name (Patient meta data file in xlsx format)', required = True)
+    required.add_argument('--inCandidateFile', metavar = "Input File", dest = "inCandidateFile", nargs = '+', help = 'Input File Name (Patient meta data file in .xlsx format)', required = True)
     required.add_argument('--inCanonicalFile', metavar = "Input File", dest = "inCanonicalFile", help = 'Canonical Transcripts file', required = True)
     required.add_argument('--inInteractome', metavar = "Input File", dest = "inInteractome", help = 'Input File Name (High-quality Interactome (.tsv) produced by Interactome.py)', required = True)
 
