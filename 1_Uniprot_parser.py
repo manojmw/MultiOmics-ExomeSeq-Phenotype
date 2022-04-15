25c256d4d35afb8d90099c87699ac4e57fff4474
manojmw <manojmonmax@gmail.com>
2022-04-15 13:56:38 +0200

    Code improvement: A single file containing all the details for a given protein. Eliminating 3 different files

--- 1_Uniprot_parser.py
+++ 1_Uniprot_parser.py
@@ -10,141 +10,218 @@ import sys
 
 ###########################################################
 
-# Parses the Uniprot file and extracts the following fields from each record:
+# Parses on STDIN a Uniprot file and extracts the following fields from each record:
 # - Primary Accession and Secondary Accession(s) from the 'AC' line
+# - Gene Name and synonyms from the 'GN' line
 # - Taxonomy Identifier from the 'OX' line
-# - ENST(s), ENSG(s) & GeneID from the 'DR' line
+# - ENST(s), ENSG(s) & GeneID(s) from the 'DR' line
 #
-# Generates 3 output files:
-# Output File 1 -> a tab-seperated file with four columns:
-# - UniProt Primary AC
-# - TaxID
-# - ENST(s)
-# - ENSG(s)
-#
-# Output File 2 -> a tab-seperated file with two columns:
-# - UniProt Secondary AC
-# - Corresponding UniProt Primary AC
-#
-# Output File 3 -> a tab-seperated file with two columns:
-# - GeneID
-# - Corresponding UniProt Primary AC
-def uniprot_parser(args):
-    try:
-        with open(args.outPrimAC, 'w') as PrimAC_outfile, open(args.outSecAC, 'w') as SecAC_outfile, open(args.outGeneID, 'w') as GeneID_outfile:
-            PrimAC_header = ['Primary_AC', 'TaxID', 'ENSTs', 'ENSGs']
-            SecAC_header = ['Primary_AC', 'Secondary_ACs']
-            GeneID_header = ['Primary_AC', 'GeneID']
-            print('\t'.join(PrimAC_header), file = PrimAC_outfile)
-            print('\t'.join(SecAC_header), file = SecAC_outfile)
-            print('\t'.join(GeneID_header), file = GeneID_outfile)
+# Prints to STDOUT in .tsv format
+# Ouput consists of 7 columns (one record per line):
+# - Uniprot Primary Accession
+# - Taxonomy Identifier
+# - ENST (or a comma seperated list of ENSTs)
+# - ENSG (or a comma seperated list of ENSGs)
+# - Uniprot Secondary Accession (or a comma seperated list of Uniprot Secondary Accessions)
+# - GeneID (or a comma seperated list of GeneIDs)
+# - Gene Name (or a comma seperated list of Gene Names)
+def uniprot_parser(UniProtinFile):
 
-            # Initializing variables/accumulators
+    UniProtinFile = sys.stdin
+
+    logging.info("Processing data from input File: %s" % UniProtinFile)
+
+    # Header line
+    UniProt_header = ['Primary_AC', 'TaxID', 'ENSTs', 'ENSGs', 'Secondary_ACs', 'GeneID', 'GeneName']
+    print('\t'.join(UniProt_header))
+
+    # Initializing variables/accumulators
+    ACs = ''
+    TaxID = 0
+    ENSTs = []
+    ENSGs = []
+    GeneIDs = []
+    GeneNames = []
+
+    # Compiling all the regular expressions
+
+    # Accession Numbers (AC), strip trailing ';'
+    re_AC = re.compile('^AC\s+(\S.*);$')
+
+    # Gene Name/Synonyms from the GN line
+    # the below regex pattern ends with either ; or , because 
+    # UniProt has a bug (at the time of writing this script) - 
+    # the lines for some records are incomplete and does not 
+    # end with semi-colon as it is supposed to and even unclosed curly braces
+    # Ex: 
+    # GN   Name=aadK {ECO:0000303|PubMed:17609790, ECO:0000303|PubMed:8293959,
+    re_GN = re.compile('^GN\s+(Name=\S.*)[,;]$')
+
+    # Organism (TaxID) from the OX line; some lines violate the uniprot spec
+    # Grab only TaxID even if additional info comes after the TaxID
+    # eg NCBI_TaxID=32201 {ECO:0000312|EMBL:ABW86978.1};
+    re_TaxID = re.compile('^OX\s+NCBI_TaxID=(\d+)[; ]')
+
+    # Ensembl transcripts and Genes from the DR line
+    re_ENS = re.compile('^DR\s+Ensembl; (\w+); \w+; (\w+)\.')
+
+    # GeneIDs from the DR line
+    re_GID = re.compile('^DR\s+GeneID;\s+(\d+);')
+
+    # Data lines
+    for line in UniProtinFile:
+        line = line.rstrip('\r\n') # removing trailing new lines and carriage returns
+
+        # Matching and retrieving the records
+        if (re_AC.match(line)):
+            if (ACs != ''):
+                # Add trailing separator to the previous entries - distingushes each AC
+                ACs += '; '
+            ACs += re_AC.match(line).group(1)
+        elif (re.match(r'^AC\s', line)):
+            # If any AC line is missed, Exit the program with an error message
+            sys.exit("Error: Missed the AC line %s\n" + line)  
+            
+        elif (re_GN.match(line)):
+            GNLine = re_GN.match(line).group(1)
+            try: 
+                # If the GN line contains any additional info other than
+                # Gene Name, each will be seperated by a ;
+                # Ex: GN   Name=Jon99Cii; Synonyms=SER1, SER5, Ser99Da; ORFNames=CG7877;
+                GNList = GNLine.split('; ')
+                if GNList:
+                    for geneinfo in GNList:
+                        # Retrieving only the Gene Name
+                        if re.match('^Name=(\S.*)', geneinfo):
+                            GeneName = re.match('^Name=(\S.*)', geneinfo).group(1)
+                            # Sometimes, Gene Name can contain additional info
+                            # such as pubmed and other accession IDs
+                            # So, we want eliminate these from our
+                            # Gene name
+                            # Ex: GN   Name=dbaA {ECO:0000303|PubMed:23001671};
+                            try:
+                                # So we split at the ' {' and keep only the
+                                # Gene Name
+                                GeneNamewithAcID = GeneName.split(' {')
+                                GeneName = GeneNamewithAcID[0]
+                            except: 
+                                GeneName = GeneName
+                                # There can be multiple 'GN' lines, especially
+                                # when there are many synonyms
+                                # So we check to avoid adding the same gene name again
+                                # Ex: 
+                                # GN   Name=Jon99Cii; Synonyms=SER1, SER5, Ser99Da; ORFNames=CG7877;
+                                # GN   Name=Jon99Ciii; Synonyms=SER2, SER5, Ser99Db; ORFNames=CG15519;
+                            if not GeneName in GeneNames:
+                                GeneNames.append(GeneName)   
+                        # retreiving synonyms for Gene Name (if it exists)            
+                        if re.match('^Synonyms=(\S.*)', geneinfo):
+                            GeneSynoinfo = re.match('^Synonyms=(\S.*)', geneinfo).group(1)
+                            GeneSynonyms = []
+                            # There can be multiple synonyms seperated by a ,
+                            # Ex: 
+                            # GN   Name=Jon99Cii; Synonyms=SER1, SER5, Ser99Da;
+                            try:
+                                GeneSynonymList = GeneSynoinfo.split(', ')
+                                if GeneSynonymList:
+                                    # Like Gene Name, even Gene synonyms can
+                                    # can contain additional info such as pubmed and other accession IDs
+                                    # So, we want eliminate these and keep only the synonym name
+                                    # Ex:
+                                    # GN   Name=Sh3bp5; Synonyms=Sab {ECO:0000303|PubMed:10339589};
+                                    for synonym in GeneSynonymList:
+                                        try:
+                                            synonym = synonym.split(' {')
+                                            synonym = synonym[0]
+                                            GeneSynonyms.append(synonym)
+                                        except:
+                                            GeneSynonyms.append(synonym)
+                            except:
+                                GeneSynonyms.append(GeneSynoinfo)
+                            # Avoid adding the same synonym again especially
+                            # when they occur on multiple 'GN' lines
+                            for synonym in GeneSynonyms:
+                                if not synonym in GeneNames:
+                                    GeneNames.append(synonym)
+            except:
+                # if the GN line contains only the Gene name
+                # Ex: GN   Name=APOM;
+                if re.match('^Name=(\S.*)', GNLine):
+                    GeneName = re.match('^Name=(\S+)', GeneNameLine).group(1)
+                    try:
+                        # When Gene Name can contains additional info
+                        GeneNamewithAcID = GeneName.split( '{')
+                        GeneName = GeneNamewithAcID[0]
+                    except:
+                        GeneName = GeneName 
+                    if not GeneName in GeneNames:
+                        GeneNames.append(GeneName)          
+        elif (re.match(r'^GN\s+Name.*', line)):
+            sys.exit("Error: Missed the Gene Name \n" + ACs + line)    
+
+        elif (re_TaxID.match(line)):
+            if (TaxID != 0):
+                sys.exit("Error: Several OX lines for the protein: \t" + ACs)
+            TaxID = re_TaxID.match(line).group(1)
+        elif (re.match(r'^OX\s',line)):
+            sys.exit("Error: Missed the OX line %s\n" + line)
+        elif (re_ENS.match(line)):
+            ENS_match = re_ENS.match(line)
+            ENSTs.append(ENS_match.group(1))
+            ENSG = ENS_match.group(2)
+            if ENSG not in ENSGs:
+                ENSGs.append(ENSG)
+        elif (re.match(r'^DR\s+Ensembl;', line)):
+            sys.exit("Error: Failed to get all the Ensembl Identifiers\n" + ACs + line)
+        elif (re_GID.match(line)):
+            GeneIDs.append(re_GID.match(line).group(1))
+        elif (re.match(r'^DR\s+GeneID.*', line)):
+            sys.exit("Error: Missed the GeneIDs \n" + ACs + line)
+
+        # '//' means End of the record
+        # we Process the retreived data
+        elif (line == '//'):
+            # ignore entry if bad species; TaxID Human = 9606
+            if (TaxID == '9606'):
+                try:
+                    ACs_split = ACs.split('; ')
+                    primary_AC = ACs_split[0] # Grab only the first AC
+                    secondary_AC_list = ACs_split[1:] # Grab the remaining ACs
+                    # storing secondary_ACs as a single comma-seperated string
+                    secondary_ACs = ','.join(secondary_AC_list) 
+                except:
+                    sys.exit('Error: Failed to store Accession IDs for the protein: \t' + ACs)
+                try:
+                    # storing Gene names as a single comma-seperated string
+                    GeneNames = ','.join(GeneNames)
+                except:
+                    sys.exit('Error: Failed to store Gene Name for the protein: \t' + ACs)    
+                try:
+                    # storing ENSTs and ENSGs as a single comma-seperated string
+                    ENSTs = ','.join(ENSTs)
+                    ENSGs = ','.join(ENSGs)
+                except:
+                    sys.exit('Error: Failed to store Ensembl Identifiers for the protein: \t' + ACs)
+                try:
+                    # storing GeneIDs as a single comma-seperated string
+                    GeneIDs = ','.join(GeneIDs)
+                except:
+                    sys.exit('Error: Failed to store Gene Identifiers for the protein: \t' + ACs)    
+
+                # Writing to the output file
+                UniProt_outline = [primary_AC, TaxID, ENSTs, ENSGs, secondary_ACs, GeneIDs, GeneNames]
+                print('\t'.join(UniProt_outline))
+
+            # Reset all accumulators and move on to the next record
             ACs = ''
             TaxID = 0
             ENSTs = []
             ENSGs = []
             GeneIDs = []
+            GeneNames = []
+            continue
 
-            # Compiling all the regular expressions
-
-            # Accession Numbers (AC), strip trailing ';'
-            re_AC = re.compile('^AC\s+(\S.*);$')
-
-            # Organism (TaxID) from the OX line; some lines violate the uniprot spec
-            # Grab only TaxID even if additional info comes after the TaxID
-            # eg NCBI_TaxID=32201 {ECO:0000312|EMBL:ABW86978.1};
-            re_TaxID = re.compile('^OX\s+NCBI_TaxID=(\d+)[; ]')
-
-            # Ensembl transcripts and Genes from the DR line
-            re_ENS = re.compile('^DR\s+Ensembl; (\w+); \w+; (\w+)\.')
-
-            # GeneIDs from the DR line
-            re_GID = re.compile('^DR\s+GeneID;\s+(\d+);')
-
-            logging.info("Processing data from UniProt File")
-
-            logging.info("Writing data to output files...")
-
-            # Data lines
-            for line in sys.stdin:
-                line = line.rstrip('\r\n') # removing trailing new lines and carriage returns
-
-                # Matching and retrieving the records
-                if (re_AC.match(line)):
-                    if (ACs != ''):
-                        # Add trailing separator to the previous entries - distingushes each AC
-                        ACs += '; '
-                    ACs += re_AC.match(line).group(1)
-                elif (re.match(r'^AC\s', line)):
-                    # If any AC line is missed, Exit the program with an error message
-                    sys.exit("Error: Missed the AC line %s\n", line)
-                elif (re_TaxID.match(line)):
-                    if (TaxID != 0):
-                        sys.exit("Error: Several OX lines for the protein: \t", ACs)
-                    TaxID = re_TaxID.match(line).group(1)
-                elif (re.match(r'^OX\s',line)):
-                    sys.exit("Error: Missed the OX line %s\n", line)
-                elif (re_ENS.match(line)):
-                    ENS_match = re_ENS.match(line)
-                    ENSTs.append(ENS_match.group(1))
-                    ENSG = ENS_match.group(2)
-                    if ENSG not in ENSGs:
-                        ENSGs.append(ENSG)
-                elif (re.match(r'^DR\s+Ensembl;', line)):
-                    sys.exit("Error: Failed to get all the Ensembl Identifiers\n", ACs, line)
-                elif (re_GID.match(line)):
-                    GeneIDs.append(re_GID.match(line).group(1))
-                elif (re.match(r'^DR\s+GeneID.*', line)):
-                    sys.exit("Error: Missed the GeneIDs \n", ACs, line)
-
-                # '//' means End of the record
-                # we Process the retreived data
-                elif (line == '//'):
-                    # ignore entry if bad species; TaxID Human = 9606, TaxID Mouse = 10090
-                    if ((TaxID == '9606') or (TaxID == '10090')):
-                        try:
-                            ACs_split = ACs.split('; ')
-                            primary_AC = ACs_split[0] # Grab only the first AC
-                            secondary_ACs = ACs_split[1:] # Grab the remaining ACs
-                        except:
-                            sys.exit('Error: Failed to store Accession IDs for the protein: \t', ACs)
-                        try:
-                            # Processing ENSTs and ENSGs
-                            ENSTs = ','.join(ENSTs)
-                            ENSGs = ','.join(ENSGs)
-                        except:
-                            sys.exit('Error: Failed to store Ensembl Identifiers for the protein: \t', ACs)
-
-                        # Writing to output files
-                        primaryAC_line = [primary_AC, TaxID, ENSTs, ENSGs]
-                        print('\t'.join(primaryAC_line), file = PrimAC_outfile)
-
-                        for secondary_AC in secondary_ACs:
-                            secondaryAC_line = [primary_AC, secondary_AC]
-                            print('\t'.join(secondaryAC_line), file = SecAC_outfile)
-
-                        for GeneID in GeneIDs:
-                            GeneID_line = [primary_AC, GeneID]
-                            print('\t'.join(GeneID_line), file = GeneID_outfile)
-
-                    # Reset all accumulators and move on to the next record
-                    ACs = ''
-                    TaxID = 0
-                    ENSTs = []
-                    ENSGs = []
-                    GeneIDs = []
-                    continue
-
-        # Closing the files
-        PrimAC_outfile.close()
-        SecAC_outfile.close()
-        GeneID_outfile.close()
-
-    except IOError as e:
-        print("Error: Unable to open the files for writing")
-
-    logging.info("Done 🎉")
+    logging.info("All Done, completed successfully!")
 
     return
 
@@ -155,37 +232,26 @@ def main():
 
     file_parser = argparse.ArgumentParser(description =
     """
----------------------------------------------------------------------------------------------
-Program: Parses a uniprot file (STDIN), processes it and produces the following output files:
----------------------------------------------------------------------------------------------
-Output File 1 (--outPrimaryAC):   A tab-seperated file (.tsv) with four columns
-                                   -> UniProt Primary Accession
-                                   -> Taxonomy Identifier
-                                   -> ENST (or Comma seperated list of ENSTs)
-                                   -> ENSG (or Comma seperated list of ENSGs)
-
-Output File 2 (--outSecondaryAC): A tab-seperated file (.tsv) with two columns
-                                   -> UniProt Secondary Accession
-                                   -> Corresponding UniProt Primary Accession
-
-Output File 3 (--outGeneID):      A tab-seperated file (.tsv) with two columns
-                                   -> GeneID
-                                   -> Corresponding UniProt Primary Accession
----------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------
+Program: Parses on STDIN a UniProt file, processes each record and prints to STDOUT in .tsv format
+--------------------------------------------------------------------------------------------------
+The output consists of 7 columns:
+ -> Uniprot Primary Accession
+ -> Taxonomy Identifier
+ -> ENST (or a comma seperated list of ENSTs)
+ -> ENSG (or a comma seperated list of ENSGs)
+ -> Uniprot Secondary Accession (or a comma seperated list of Uniprot Secondary Accessions)
+ -> GeneID (or a comma seperated list of GeneIDs)
+ -> Gene Name (or a comma seperated list of Gene Names)
+--------------------------------------------------------------------------------------------------
 
 Arguments [defaults] -> Can be abbreviated to shortest unambiguous prefixes
     """,
     formatter_class = argparse.RawDescriptionHelpFormatter)
 
-    required = file_parser.add_argument_group('Required arguments')
-    optional = file_parser.add_argument_group('Optional arguments')
-
-    required.add_argument('--outPrimAC',  metavar = "Output File", dest = "outPrimAC", help = 'Primary Accession File with ENSTs, ENSGs & TaxID', required = True)
-    required.add_argument('--outSecAC', metavar = "Output File", dest = "outSecAC", help = 'Secondary Accession File', required = True)
-    required.add_argument('--outGeneID', metavar = "Output File", dest = "outGeneID", help = 'GeneID File', required = True)
-
+    file_parser.set_defaults(func=uniprot_parser)
     args = file_parser.parse_args()
-    uniprot_parser(args)
+    args.func(args)
 
 if __name__ == "__main__":
     # Logging to Standard Error
