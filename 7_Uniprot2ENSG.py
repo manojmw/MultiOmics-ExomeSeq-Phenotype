23c80fe4ee4bc52d4b19b3136b728b477d4f3d65
manojmw <manojmonmax@gmail.com>
2022-04-28 19:07:25 +0200

    7_Uniprot2ENSG.py: Adapting code to the new output of UniProt parser

--- 7_Uniprot2ENSG.py
+++ 7_Uniprot2ENSG.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python
 
 import sys, argparse
-import re
 import logging
-import time
 
 ###########################################################
 
@@ -13,6 +11,8 @@ import time
 # Returns a dictionary:
 # Key -> ENSG; Value -> Gene
 def ENSG_Gene(inCanonicalFile):
+
+    logging.info("Starting to run...")
 
     ENSG_Gene_dict = {} # Initializing an empty dictionary
 
@@ -66,95 +66,57 @@ def Uniprot2ENSG(args):
     # Calling the function ENSG_Gene
     ENSG_Gene_dict = ENSG_Gene(args.inCanonicalFile)
 
-    # INPUT -> Uniprot Primary_AC File
-    UniprotPrimAC_File = open(args.inPrimAC)
+    Uniprot_File = open(args.inUniProt)
 
-    UniprotPrimACFile_header_line = UniprotPrimAC_File.readline() # Grabbing the header line
+    # Grabbing the header line
+    Uniprot_header = Uniprot_File.readline()
 
-    UniprotPrimACFile_header_line = UniprotPrimACFile_header_line.strip('\n')
+    Uniprot_header = Uniprot_header.rstrip('\n')
 
-    UniprotPrimACFile_header_fields = UniprotPrimACFile_header_line.split('\t')
+    Uniprot_header_fields = Uniprot_header.split('\t')
 
     # Check the column headers and grab indexes of our columns of interest
-    (UniProt_PrimAC_col, ENSG_col) = (-1,-1)
+    (UniProt_PrimAC_index, ENSG_index) = (-1, -1)
 
-    for header in UniprotPrimACFile_header_fields:
-        if header == 'Primary_AC':
-            UniProt_PrimAC_col = UniprotPrimACFile_header_fields.index(header)
-        elif header == 'ENSGs':
-            ENSG_col = UniprotPrimACFile_header_fields.index(header)
+    for i in range(len(Uniprot_header_fields)):
+        if Uniprot_header_fields[i] == 'Primary_AC':
+            UniProt_PrimAC_index = i
+        elif Uniprot_header_fields[i] == 'ENSGs':
+            ENSG_index = i
 
-    if not UniProt_PrimAC_col >= 0:
-        sys.exit("Missing required column title: 'Primary_AC' in the file: %s \n" % inPrimAC)
-    elif not ENSG_col >= 0:
-        sys.exit("Missing required column title: 'ENSGs' in the file: %s \n" % inPrimAC)
+    if not UniProt_PrimAC_index >= 0:
+        logging.error("At Step 5.2_addInteractome - Missing required column title 'Primary_AC' in the file: %s \n" % inUniProt)
+        sys.exit()
+    elif not ENSG_index >= 0:
+        logging.error("At Step 5.2_addInteractome - Missing required column title 'ENSG' in the file: %s \n" % inUniProt)
+        sys.exit()
     # else grabbed the required column indexes -> PROCEED
 
-    # Compiling regular expressions###
 
-    # Eliminating Mouse ENSGs
-    re_ENSMUST = re.compile('^ENSMUSG')
-
-    #Counter for Human Uniprot Primary Accession
-    Count_HumanUniprotPrimAC = 0
-
-    # Counter for canonical ENSGs
-    canonical_ENSG_count = 0
-
-    # Counter for accessions with no canonical human ENSG
-    no_CanonicalHumanENSG = 0
-
-    # Counter for accessions with single canonical human ENSG
-    single_CanonicalHumanENSG = 0
-
-    # Counter for accessions with multiple canonical human ENSGs
-    multiple_CanonicalHumanENSG = 0
-
-    # Parsing the Uniprot Primary Accession file
-    for line in UniprotPrimAC_File:
+    # Data lines
+    for line in Uniprot_File:
         line = line.rstrip('\n')
-        UniprotPrimAC_fields = line.split('\t')
+        Uniprot_fields = line.split('\t')
 
         # ENSG column  - This is a single string containing comma-seperated ENSGs
         # So we split it into a list that can be accessed later
-        UniProt_ENSGs = UniprotPrimAC_fields[ENSG_col].split(',')
-
-        # Initializing empty lists
-        human_ENSGs = []
+        UniProt_ENSGs = Uniprot_fields[ENSG_index].split(',')
+        
         canonical_human_ENSGs = []
 
-        # Eliminating Mouse ENSGs
-        for UniProt_ENSG in UniProt_ENSGs:
-            if not re_ENSMUST.match(UniProt_ENSG):
-                human_ENSGs.append(UniProt_ENSG)
-        if not human_ENSGs:
-            continue
-        Count_HumanUniprotPrimAC += 1
-
-        for ENSG in human_ENSGs:
+        # If ENSG is in the canonical transcripts file
+        # Append it to canonical_human_ENSGs
+        for ENSG in UniProt_ENSGs:
             if ENSG in ENSG_Gene_dict.keys():
                 canonical_human_ENSGs.append(ENSG)
-                canonical_ENSG_count += 1
 
-        # Key -> Uniprot Primary accession
-        # Value -> Canonical_human_ENSG
+        # Keeping the count of protein with single ENSGs
+        if len(canonical_human_ENSGs) == 1:
+            print(Uniprot_fields[UniProt_PrimAC_index], "\t", ''.join(canonical_human_ENSGs))
 
-        # After eliminating mouse and non-canonical ENSGs, some values can be empty
-        # So, we keep a count of these accessions
-        if len(canonical_human_ENSGs) == 0:
-            no_CanonicalHumanENSG += 1
-        elif len(canonical_human_ENSGs) == 1:
-            print(UniprotPrimAC_fields[UniProt_PrimAC_col], '\t', ''.join(canonical_human_ENSGs))
-            single_CanonicalHumanENSG += 1
-        elif len(canonical_human_ENSGs) > 1:
-            multiple_CanonicalHumanENSG += 1
+    Uniprot_File.close()
 
-    logging.debug("\nTotal no. of Human UniProt Primary Accessions: %d " % Count_HumanUniprotPrimAC)
-    logging.debug("\nTotal no. of ENSGs in the Canonical Transcripts file: %d " % len(ENSG_Gene_dict.keys()))
-    logging.debug("\nTotal no. of ENSGs in the UniProt Primary Accession file: %d " % canonical_ENSG_count)
-    logging.debug("\nNo. of UniProt primary accessions without canonical human ENSG: %d " % no_CanonicalHumanENSG)
-    logging.debug("\nNo. of UniProt primary accessions with single canonical human ENSG: %d " % single_CanonicalHumanENSG)
-    logging.debug("\nNo. of UniProt primary accessions with multiple canonical human ENSGs: %d " % multiple_CanonicalHumanENSG)
+    logging.info("All done, completed successfully!")
 
     return
 
@@ -165,7 +127,8 @@ def main():
     file_parser = argparse.ArgumentParser(description =
     """
 ---------------------------------------------------------------------------------------------------------------------------------
-Program: Parses the Uniprot Primary Accession file produced by uniprot_parser, maps to canonical transcripts and prints to STDOUT
+Program: Parses the Uniprot file (produced by 1_Uniprot_parser.py) and the canonical transcripts file, maps the Uniprot Primary 
+         Accessions to ENSG and prints to STDOUT 
 ---------------------------------------------------------------------------------------------------------------------------------
 The output consists of 2 columns in .tsv format:
  -> Uniprot Primary Accession
@@ -177,15 +140,14 @@ The output consists of 2 columns in .tsv format:
     required = file_parser.add_argument_group('Required arguments')
     optional = file_parser.add_argument_group('Optional arguments')
 
-    required.add_argument('--inPrimAC', metavar = "Input File", dest = "inPrimAC", help = 'Uniprot Primary Accession File generated by the uniprot parser', required = True)
+    required.add_argument('--inUniProt', metavar = "Input File", dest = "inUniprot", help = 'Uniprot output File generated by the UniProt_parser.py', required = True)
     required.add_argument('--inCanonicalFile', metavar = "Input File", dest = "inCanonicalFile", help = 'Canonical Transcripts file', required = True)
 
     args = file_parser.parse_args()
     Uniprot2ENSG(args)
 
 if __name__ == "__main__":
-    # Logging to the file
-    date = time.strftime("%Y_%m_%d-%H%M%S")
-    Log_Format = "%(levelname)s %(asctime)s - %(message)s \n"
-    logging.basicConfig(filename ='Uniprot2ENSG_%s.log' % date, filemode = 'a', format  = Log_Format, level = logging.DEBUG)
+    # Logging to Standard Error
+    Log_Format = "%(levelname)s - %(asctime)s - %(message)s \n"
+    logging.basicConfig(stream = sys.stderr, format  = Log_Format, level = logging.DEBUG)
     main()
